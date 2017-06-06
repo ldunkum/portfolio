dcc68c00469972587d25761ee58636c051590e6c
lDunkum <luis.silvan365@gmail.com>
2017-06-06 12:46:51 +0100

    show image on first click

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+var initHidden = false;
+
 document.getElementById("image-loader").addEventListener("click", function(){
     var picture = document.getElementById('picture');
+    if(!initHidden){
+        picture.style.visibility = 'hidden';
+        initHidden = true;
+    }
     if (picture.style.visibility === 'hidden') {
         picture.style.visibility = 'visible';
     } else {
