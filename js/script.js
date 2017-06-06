e048e1c5aca0512a6c19d852afd8353867c79fe1
lDunkum <luis.silvan365@gmail.com>
2017-06-06 12:39:50 +0100

    removed alert

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 document.getElementById("image-loader").addEventListener("click", function(){
     var picture = document.getElementById('picture');
     if (picture.style.visibility === 'hidden') {
-        alert('hj');
         picture.style.visibility = 'visible';
     } else {
         picture.style.visibility = 'hidden';
