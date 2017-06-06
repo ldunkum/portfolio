7e9376e96037469fd6df195529a7bf7af6607f14
lDunkum <luis.silvan365@gmail.com>
2017-06-06 13:16:55 +0100

    image link only shows with javascript enabled

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 var initHidden = false;
 
-document.getElementById("image-loader").addEventListener("click", function(){
-    var picture = document.getElementById('picture');
-    if(!initHidden){
-        picture.style.visibility = 'hidden';
-        initHidden = true;
-    }
-    if (picture.style.visibility === 'hidden') {
-        picture.style.visibility = 'visible';
-    } else {
-        picture.style.visibility = 'hidden';
-    }
-});
+window.onload = function(){
+    var textWrapper = document.getElementById("show-picture-text");    
+    var showPictureText = "If you're curious to see what I look like, click <span id='image-loader'>here</span>.";
+    textWrapper.innerHTML = showPictureText;
+    clickListener();
+
+};
+
+var clickListener = function() { 
+    document.getElementById("image-loader").addEventListener("click", function(){
+        var picture = document.getElementById('picture');
+        if(!initHidden){
+            picture.style.visibility = 'hidden';
+            initHidden = true;
+        }
+        if (picture.style.visibility === 'hidden') {
+            picture.style.visibility = 'visible';
+        } else {
+            picture.style.visibility = 'hidden';
+        }
+    });
+};
